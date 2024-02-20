58e967bc2e823a82f887c21cacecab8f30fd56a4
George Thomas Madeley <george.madeley@outlook.com>
2024-02-20 13:18:25 +0000

    Moved Function to Separate Files

---
+++ cam_settings.m
@@ -0,0 +1,29 @@
+function [cam, webcamName] = cam_settings()
+    % Getting then printing a list of available webcams
+    camList = webcamlist;
+    disp(camList);
+
+    % Ask to enter is camera name and check if that name is valid. Else,
+    % try again.
+    webcamName = "";
+    validWebcam = false;
+    while ~validWebcam
+        % Ask to enter camera name
+        disp("Enter Desired Webcam.")
+        webcamName = input(">?", "s");
+        validWebcam = ismember(webcamName, camList);
+        if ~validWebcam
+            disp("The name you have entered is not in the list of available webcams.")
+        end
+    end
+
+    % Connect to Webcam
+    cam = webcam(webcamName);
+
+    % Display image
+    preview(cam)
+
+    % display webcam properties
+    disp("Webcam Properties:")
+    disp(cam)
+end
--- capture_images.m
+++ capture_images.m
@@ -1,34 +1,4 @@
-webcamName = camSettings();
-captureImages('D:\FYP\snapshots', webcamName);
-
-function webcamName = camSettings()
-    % Getting then printing a list of available webcams
-    camList = webcamlist;
-    disp(camList);
-
-    % Ask to enter is camera name and check if that name is valid. Else,
-    % try again.
-    webcamName = "";
-    validWebcam = false;
-    while ~validWebcam
-        % Ask to enter camera name
-        disp("Enter Desired Webcam.")
-        webcamName = input(">?", "s");
-        validWebcam = ismember(webcamName, camList);
-        if ~validWebcam
-            disp("The name you have entered is not in the list of available webcams.")
-        end
-    end
-
-    % Connect to Webcam
-    cam = webcam(webcamName);
-
-    % display webcam properties
-    disp("Webcam Properties:")
-    disp(cam)
-end
-
-function captureImages(savepath, webcamName)
+function capture_images(savepath, webcamName)
     % Validate arguments
     arguments
         savepath (1,1) string = 'D:\FYP\snapshots'
