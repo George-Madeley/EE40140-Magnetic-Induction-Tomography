2cbd76dd39dfc4b7f976f91662c0cf9e83a562e4
George Thomas Madeley <george.madeley@outlook.com>
2024-02-20 11:04:33 +0000

    Added Feature to Save Multiple Images

--- data_collection.m
+++ data_collection.m
@@ -1,6 +1,9 @@
 % Setting Directory for snapshots
 savepath = 'D:\FYP\snapshots';
 
+% Creating naming pattern for images
+nametemplate = 'snapshot_%05d.png';
+
 % Getting then printing a list of available webcams
 camList = webcamlist;
 disp(camList);
@@ -8,12 +11,17 @@ disp(camList);
 % Connect to Webcam
 cam = webcam('HD Pro Webcam C920');
 
-% Take snapshot
-img = snapshot(cam);
+for imNum = 1 : 5
+    % Take snapshot
+    img = snapshot(cam);
+    
+    % Create filename
+    filename = sprintf(nametemplate, imNum);
+    filename = fullfile(savepath, filename);
 
-% Save image
-filename = fullfile(savepath, "snapshot.png");
-imwrite(img, filename);
+    % Save image
+    imwrite(img, filename);
+end
 
 % Disconnecting from Webcam and clearing variable
 clear cam;
