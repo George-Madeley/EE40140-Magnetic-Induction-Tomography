95065b7415f04c4a681b92347a5436fa85336665
George Thomas Madeley <george.madeley@outlook.com>
2024-03-01 10:41:21 +0000

    Updated Image Generation to Use new CSV

--- scripts/image_generation/main.py
+++ scripts/image_generation/main.py
@@ -14,11 +14,18 @@ def main(downScaleFactor=1, approximations=(0.01, 0.99)):
     newHeight = originalHeight // downScaleFactor
 
     # load in input_data.csv
-    df = pd.read_csv('input_data.csv')
+    df = pd.read_csv('data.csv')
 
-    # Get the voltage data
-    column_names = df.filter(regex='sensor_\d+').columns
+    # Get the background voltage and the sensor voltage columns
+    bb_names = df.filter(regex='bb_\d+').columns
+    cc_names = df.filter(regex='cc_\d+').columns
+    # combine the two lists and return the result
+    column_names = bb_names.append(cc_names)
+
     voltages = df[column_names].values
+
+    # get the number of columns in the voltages array
+    num_columns = voltages.shape[1]
 
     # Normalize the voltages by dividing by 2e4
     voltages = voltages / 2e4
@@ -27,15 +34,15 @@ def main(downScaleFactor=1, approximations=(0.01, 0.99)):
 
     for i, row in df.iterrows():
         # Get the filename
-        imageFilename = row['filepath']
+        imageFilename = row['cc_filename']
         
-        imageFilePath = os.path.join(f'./images/processed/{newHeight}x{newWidth}', imageFilename)
+        imageFilePath = os.path.join(f'./images/processed/{newWidth}x{newHeight}', imageFilename)
 
         # Read the .png file
         image = plt.imread(imageFilePath)
 
-        # Average the four channels
-        image = np.mean(image, axis=2)
+        # Average the first three channels
+        image = np.mean(image[:, :, :3], axis=2)
 
         # Find all the zero values and replace them with 0.01
         zero_values = image == 0
@@ -51,16 +58,17 @@ def main(downScaleFactor=1, approximations=(0.01, 0.99)):
 
     # Define the model architecture
     model = tf.keras.Sequential([
-        tf.keras.layers.Reshape((120, 1), input_shape=(120,)),
+        tf.keras.layers.Reshape((num_columns, 1), input_shape=(num_columns,)),
         tf.keras.layers.Conv1D(filters=32, kernel_size=3, activation='relu'),
         tf.keras.layers.Conv1D(filters=64, kernel_size=3, activation='relu'),
+        tf.keras.layers.Conv1D(filters=128, kernel_size=3, activation='relu'),
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(newHeight * newWidth, activation='sigmoid'),
-        tf.keras.layers.Reshape((newHeight, newWidth))
+        tf.keras.layers.Reshape((newWidth, newHeight))
     ])
 
     # Compile the model
-    model.compile(optimizer='adam', loss='binary_crossentropy')
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
     # Train the model
     model.fit(
@@ -78,7 +86,7 @@ def main(downScaleFactor=1, approximations=(0.01, 0.99)):
 
     # run the model using the last row of the input data
     test_image = voltages[-1]
-    test_image = test_image.reshape(1, 120)
+    test_image = test_image.reshape(1, num_columns)
     predicted_image = model.predict(test_image)
     plt.imshow(predicted_image[0], cmap='gray')
     plt.show()
@@ -86,6 +94,6 @@ def main(downScaleFactor=1, approximations=(0.01, 0.99)):
 
 
 if __name__ == '__main__':
-    main()
+    main(downScaleFactor=8, approximations=(0.01, 0.99))
 
     
