8417bdcce72667b7628474ae9d64411a8e3c55ae
George Thomas Madeley <george.madeley@outlook.com>
2024-04-25 10:56:39 +0100

    Added Mean Score Time Plot

--- scripts/graphing/plotPerformance2D.py
+++ scripts/graphing/plotPerformance2D.py
@@ -65,57 +65,73 @@ def plotPerformance2D(model: str) -> None:
 
     # cmap options: 'plasma', 'binary', 'YlGn'
 
-    # Check if the features are in the data.
-    if x_feature not in data.columns:
-      print(f"Feature {x_feature} not in data.")
-      return
-    if y_feature not in data.columns:
-      print(f"Feature {y_feature} not in data.")
-      return
-    if hue not in data.columns:
-      print(f"Feature {hue} not in data.")
-      return
-    
-    # find the min, max and mean of the accuracy for each combination of x and y
-    data = data.groupby([x_feature, y_feature]).agg({hue: ['min', 'max', 'mean']}).reset_index()
-    data.columns = [x_feature, y_feature, 'min', 'max', 'mean']
-
-    # remove rows where max_depth is 1
-    data = data[data[x_feature] != 1].reset_index(drop=True)
-
-    # create a dummy scatter plot to define a mappable for the colorbar creation
-    dummy_plot = plt.scatter([], [], c=[], cmap=cmap)
-
-    # plot a scatter plot with the mean accuracy
-    sns.scatterplot(data=data, x=x_feature, y=y_feature, hue=stat, palette=cmap, s=size)
-    plt.xlabel(formatString(x_feature))
-    plt.ylabel(formatString(y_feature))
-    plt.gca().set_xticklabels([formatString(label.get_text()) for label in plt.gca().get_xticklabels()])
-    plt.gca().set_yticklabels([formatString(label.get_text()) for label in plt.gca().get_yticklabels()])
-    plt.title(f'{formatString(model)} Performance')
-    plt.legend().remove()
-    
-    if indicators:
-      # Label each point with its value
-      for i in range(len(data)):
-        plt.text(data[x_feature][i], data[y_feature][i], round(data[stat][i], 2), ha='center', va='bottom')
-    
-    # Add a color bar
-    plt.colorbar(dummy_plot, label=hue)
-    plt.clim(data[stat].min(), data[stat].max())
-
-    plt.tight_layout()
-
     print('\n\n Filename: ')
     fileName = str(input('>?\t'))
 
+    plt_accuracy = graphPlot(x_feature, y_feature, 'Accuracy', data, model, indicators, stat, size, cmap)
     if fileName != '':
-        filePath = os.path.join('images', 'graphs', f'{fileName}.png')
-        plt.savefig(filePath)
+        filePath = os.path.join('images', 'graphs', f'{fileName} - Accuracy.png')
+        plt_accuracy.savefig(filePath)
     else:
-        plt.show()
+        plt_accuracy.show()
 
-    plt.close()
+    plt.close('all')
+
+
+    plt_time = graphPlot(x_feature, y_feature, 'mean_score_time', data, model, indicators, stat, size, cmap + '_r')
+    if fileName != '':
+        filePath = os.path.join('images', 'graphs', f'{fileName} - Time.png')
+        plt_time.savefig(filePath)
+    else:
+        plt_time.show()
+    
+
+
+    plt.close('all')
+
+def graphPlot(x_feature, y_feature, hue, data, model, indicators=False, stat='mean', size=200, cmap='RdYlGn'):
+  # Check if the features are in the data.
+  if x_feature not in data.columns:
+    print(f"Feature {x_feature} not in data.")
+    return
+  if y_feature not in data.columns:
+    print(f"Feature {y_feature} not in data.")
+    return
+  if hue not in data.columns:
+    print(f"Feature {hue} not in data.")
+    return
+  
+  # find the min, max and mean of the accuracy for each combination of x and y
+  data = data.groupby([x_feature, y_feature]).agg({hue: ['min', 'max', 'mean']}).reset_index()
+  data.columns = [x_feature, y_feature, 'min', 'max', 'mean']
+
+  # remove rows where max_depth is 1
+  data = data[data[x_feature] != 1].reset_index(drop=True)
+
+  # create a dummy scatter plot to define a mappable for the colorbar creation
+  dummy_plot = plt.scatter([], [], c=[], cmap=cmap)
+
+  # plot a scatter plot with the mean accuracy
+  sns.scatterplot(data=data, x=x_feature, y=y_feature, hue=stat, palette=cmap, s=size)
+  plt.xlabel(formatString(x_feature))
+  plt.ylabel(formatString(y_feature))
+  plt.gca().set_xticklabels([formatString(label.get_text()) for label in plt.gca().get_xticklabels()])
+  plt.gca().set_yticklabels([formatString(label.get_text()) for label in plt.gca().get_yticklabels()])
+  plt.title(f'{formatString(model)} {formatString(hue)}')
+  plt.legend().remove()
+  
+  if indicators:
+    # Label each point with its value
+    for i in range(len(data)):
+      plt.text(data[x_feature][i], data[y_feature][i], round(data[stat][i], 2), ha='center', va='bottom')
+  
+  # Add a color bar
+  plt.colorbar(dummy_plot, label=formatString(hue))
+  plt.clim(data[stat].min(), data[stat].max())
+
+  plt.tight_layout()
+
+  return plt
 
 def formatString(string: str) -> str:
     """
