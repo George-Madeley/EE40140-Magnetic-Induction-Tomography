47d148d0577a60a97126fd3d913eeb9a2aad404b
George Thomas Madeley <george.madeley@outlook.com>
2024-05-03 16:17:41 +0100

    Improved Shape Count Script

---
+++ scripts/graphing/plotShapeNum.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import os
+
+def plotShapeNum():
+  # Read the CSV file
+  data = pd.read_csv(os.path.join('data', 'data_samples.csv'))
+
+  # Make sure the 'sample' column is of type string
+  data['sample'] = data['sample'].astype(str)
+
+  # Group records by the 'sample' column and count the occurrences
+  sample_counts = data.groupby('sample').size()
+
+  # Define the colors for the stacked bars
+  colors = ['black', 'blue', 'orange']
+
+  # Filter the data for samples with 'H' or copperer
+  copper_samples = data[data['sample'] >= 'H']
+  copper_sample_counts = copper_samples.groupby('shape').size()
+
+  # Filter the data for samples higher than 'H'
+  aluminium_samples = data[data['sample'] < 'H']
+
+  # pop samples from aluminium samples that are '0'
+  null_samples = aluminium_samples[aluminium_samples['sample'] == '0']
+  null_samples_counts = null_samples.groupby('shape').size()
+
+  aluminium_samples = aluminium_samples[aluminium_samples['sample'] != '0']
+  aluminium_sample_counts = aluminium_samples.groupby('shape').size()
+
+  # Plot the stacked bar chart
+  plt.bar(null_samples_counts.index, null_samples_counts, color=colors[0], label='Null Samples')
+  plt.bar(aluminium_sample_counts.index, aluminium_sample_counts, color=colors[1], label='Aluminium Samples')
+  plt.bar(copper_sample_counts.index, copper_sample_counts, bottom=aluminium_sample_counts, color=colors[2], label='Copper Samples')
+
+  # Add labels and legend
+  plt.xlabel('Shape')
+  plt.ylabel('Number of Records')
+  plt.title('Shape Counts')
+
+  # plot a key
+  plt.legend()
+  
+  # Save the plot to a file
+  save_path = os.path.join('images', 'graphs', 'shape - counts.png')
+  plt.savefig(save_path, dpi=300, bbox_inches='tight')
+  plt.close()
+
+if __name__ == '__main__':
+  # Call the function to generate the plot
+  plotShapeNum()
