93f8e54c3acf5bac3d223550255093460d34cb15
George Thomas Madeley <george.madeley@outlook.com>
2024-05-03 15:26:05 +0100

    Improved Overlapping Image Script

--- scripts/graphing/overlapImages.py
+++ scripts/graphing/overlapImages.py
@@ -2,29 +2,57 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-def overlapImages():
-    """
-    Overlaps multiple images from the 'images' directory and displays the result.
-    Each image is converted to grayscale and then overlapped using alpha blending.
-    The resulting overlapped image is displayed using matplotlib.
-    """
-    image_dir = "./images"
-    images = os.listdir(image_dir)
-    alpha = 1 / len(images)
+from utils import getTerminalArgs, formatString
 
-    fig, ax = plt.subplots()
 
-    total_img = np.zeros((480, 640))
+def overlapImages(
+    down_scale_factor: int = 32,
+    verbose: bool = False,
+    cmap: str = 'viridis',
+  ):
+  width = 640 // down_scale_factor
+  height = 480 // down_scale_factor
 
-    for image_file in images:
-        image_path = os.path.join(image_dir, image_file)
-        rgb_img = np.array(plt.imread(image_path))
-        bw_img = 0.2126 * rgb_img[:, :, 0] + 0.7152 * rgb_img[:, :, 1] + 0.0722 * rgb_img[:, :, 2]
-        bw_img = 1 - bw_img
-        total_img += bw_img * alpha
 
-    ax.imshow(total_img, cmap='gray')
 
+  image_dir = os.path.join(
+      'images',
+      'processed',
+      f'{height}x{width}')
+
+  images = os.listdir(image_dir)
+
+  # Set the alpha value for each image
+  alpha = 1 / len(images)
+
+  fig, ax = plt.subplots()
+
+  total_img = np.zeros((height, width))
+
+  for image_file in images:
+    image_path = os.path.join(image_dir, image_file)
+    rgb_img = np.array(plt.imread(image_path))
+    bw_img = 0.2126 * rgb_img[:, :, 0] + 0.7152 * \
+        rgb_img[:, :, 1] + 0.0722 * rgb_img[:, :, 2]
+    bw_img = 1 - bw_img
+    total_img += bw_img * alpha
+
+  im = ax.imshow(total_img, cmap=cmap)
+  cbar = fig.colorbar(im)
+  cbar.set_label(formatString('Probability of Pixel Occupancy'))
+  plt.axis('off')
+  plt.title(formatString(f'Distribution of {len(images)} images\n{height}x{width} pixels'))
+  plt.tight_layout()
+
+  if verbose:
     plt.show()
 
-overlapImages()
+  save_dir = os.path.join('images', 'graphs', 'overlap')
+  os.makedirs(save_dir, exist_ok=True)
+  save_path = os.path.join(save_dir, f'overlap - {height}x{width}.png')
+  plt.savefig(save_path)
+
+
+if __name__ == '__main__':
+  args = getTerminalArgs(['int', 'bool', 'str'])
+  overlapImages(*args)
