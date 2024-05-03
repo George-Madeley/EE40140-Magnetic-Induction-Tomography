d70d7e473952b51a65a549d4b9fb357aff6df131
George Thomas Madeley <george.madeley@outlook.com>
2024-05-03 15:44:47 +0100

    Improved Performance 1D Script

--- scripts/graphing/plotPerformance1D.py
+++ scripts/graphing/plotPerformance1D.py
@@ -2,20 +2,27 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
+from utils import formatString
+
 import os
 import warnings
 
 
-def plotPerformance1D(model: str) -> None:
+def plotPerformance1D(
+    model: str,
+    verbose: bool = False,
+  ) -> None:
   # Get a list of all the files in the directory 'results'
   # directory that starts with the model name.
-  files = os.listdir('results')
+  directory = os.path.join('results', 'classification')
+  files = os.listdir(directory)
   files = [f for f in files if f.startswith(model)]
+  dfs = [pd.read_csv(os.path.join(directory, f)) for f in files]
 
-  # Create a list of all the dataframes.
-  dfs = [pd.read_csv(os.path.join('results', f)) for f in files]
+  if len(dfs) == 0:
+    print(f'No data found for {model}')
+    return
 
-  # Concatenate all the dataframes.
   data = pd.concat(dfs)
 
   # get the column names between 'num_samples' and 'Accuracy'
@@ -23,32 +30,34 @@ def plotPerformance1D(model: str) -> None:
     data.columns.get_loc('numSamples') + 1:data.columns.get_loc('Accuracy')
   ]
 
-  graphDirectory = os.path.join('images', 'graphs')
+  graph_directory = os.path.join('images', 'graphs', 'performance 1D')
+  os.makedirs(graph_directory, exist_ok=True)
 
   plotGraph(
       model,
       data,
       features,
-      graphDirectory,
+      graph_directory,
       metric='mean_score_time',
-      cmap='plasma',
-      units=' (s)')
+      units=' (s)',
+      verbose=verbose
+  )
   # Get the 5% and 95% quantiles of the accuracy
   quantiles = data['Accuracy'].quantile([0.1, 0.95])
   data = data[(data['Accuracy'] > quantiles[0.1]) &
               (data['Accuracy'] < quantiles[0.95])]
-  plotGraph(model, data, features, graphDirectory, metric='Accuracy')
+  plotGraph(model, data, features, graph_directory, metric='Accuracy')
 
 
 def plotGraph(
-  model,
-  data,
-  features,
-  graphDirectory,
-  metric,
-  cmap='plasma',
-  units='',
-  font_size=16
+  model: str,
+  data: pd.DataFrame,
+  features: list,
+  graphDirectory: str,
+  metric: str,
+  units: str = '',
+  font_size: int = 16,
+  verbose: bool = False,
 ):
   for i, feature in enumerate(features):
 
@@ -78,10 +87,11 @@ def plotGraph(
       ]
     }).reset_index()
 
-    # print the summary
-    print(f'({letter}) - {formatString(model)} {formatString(metric)} by {formatString(feature)}')
-    print(summary)
-    print('\n')
+
+    if verbose:
+      print(f'({letter}) - {formatString(model)} {formatString(metric)} by {formatString(feature)}')
+      print(summary)
+      print('\n')
 
 
     warnings.filterwarnings("ignore")
@@ -90,9 +100,7 @@ def plotGraph(
     sns.boxplot(
       x=feature,
       y=metric,
-      # hue='material',
       data=data,
-      palette=cmap,
     )
     plt.xlabel(
       formatString(feature),
@@ -121,24 +129,6 @@ def plotGraph(
     plt.savefig(filepath)
     # plt.show()
     plt.close()
-
-
-def formatString(string: str) -> str:
-  """
-  Formats a string to be more readable.
-
-  Args:
-      string (str): The string to format.
-
-  Returns:
-      str: The formatted string.
-  """
-  # if there is a captial letter in the string that is not the first letter
-  # add a space before it
-  for i in range(1, len(string)):
-    if string[i].isupper() and string[i - 1] != ' ':
-      string = string[:i] + ' ' + string[i:]
-  return string.replace('_', ' ').title()
 
 
 if __name__ == '__main__':
