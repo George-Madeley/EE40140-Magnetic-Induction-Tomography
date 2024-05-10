26ba9cfdbbdc75a0361649ee36080cc90ea21503
George Thomas Madeley <george.madeley@outlook.com>
2024-05-10 13:11:00 +0100

    Plot Performance by Area

--- scripts/graphing/plotPerformanceByArea.py
+++ scripts/graphing/plotPerformanceByArea.py
@@ -15,13 +15,15 @@ def plotPerformanceByArea(
   height = 480 // down_scale_factor
 
 
-  directory = os.path.join('results', 'generation', 'per image')
+  directory = os.path.join('results', 'generation', material, 'per image')
   files = os.listdir(directory)
   files = [f for f in files if f.endswith('.csv')]
   files = [os.path.join(directory, f) for f in files]
   df_performance = pd.concat([pd.read_csv(f) for f in files])
 
-  data_path = os.path.join('data', f'data_samples_{material}.csv')
+  sample_material = 'iron' if material == 'aluminium' else material
+
+  data_path = os.path.join('data', f'data_samples_{sample_material}.csv')
   df_data = pd.read_csv(data_path)
 
   groups = df_performance.groupby('Label')
@@ -57,7 +59,7 @@ def plotPerformanceByArea(
 
     bias = total_img
 
-    images = images / bias
+    # images = images / bias
 
     # replace NaN with 0
     images = np.nan_to_num(images)
@@ -68,10 +70,22 @@ def plotPerformanceByArea(
     diff2 = ((width - height) // 2) + 1
     images = images[0][:, diff1:width - diff2]
 
+    # find the min value that is not 0
+    nonzeros_location = np.nonzero(images)
+    if nonzeros_location[0].size == 0 and nonzeros_location[1].size == 0:
+      min_val = np.min(images)
+    else:
+      min_val = np.min(images[np.nonzero(images)])
+
+    # replace 0 with the min value
+    images[images == 0] = min_val
+    
+
     fig, ax = plt.subplots()
+    fig.set_size_inches(4, 4)
     im = ax.imshow(images, cmap='viridis')
     cbar = fig.colorbar(im)
-    cbar.set_label('Probability of Pixel Occupancy')
+    cbar.set_label('Probability of Lower MAE')
     plt.axis('off')
     plt.title(f'{label} - {metric} - {height}x{width} pixels')
     plt.tight_layout()
@@ -81,9 +95,13 @@ def plotPerformanceByArea(
 
     save_dir = os.path.join('images', 'graphs', 'performance by area')
     os.makedirs(save_dir, exist_ok=True)
-    save_path = os.path.join(save_dir, f'{label} - {metric} - {height}x{width}.png')
+    save_path = os.path.join(save_dir, f'{label} - {metric} - {down_scale_factor} - {height}x{width}.png')
     plt.savefig(save_path)
 
 if __name__ == '__main__':
-  plotPerformanceByArea()
+  plotPerformanceByArea(
+    down_scale_factor=8,
+    material='copper',
+    metric='MAE'
+  )
   
