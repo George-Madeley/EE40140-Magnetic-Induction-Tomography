b564ca56a2b6524ed0f0b61b3cfcc27c378aab0c
George Thomas Madeley <george.madeley@outlook.com>
2024-05-20 12:42:56 +0100

    Updated Best Generator Scripts

--- scripts/graphing/plotBestGenerator.py
+++ scripts/graphing/plotBestGenerator.py
@@ -5,50 +5,24 @@ import pandas as pd
 import os
 
 def plotBestGenerator(
-    metrics: list[str] = ['BCE Loss', 'BCELogits Loss', 'MSE Loss', 'MAE Loss', 'S S I M Loss'],
+    metrics: list[str] = ['BCE Loss', 'BCELogits Loss', 'MSE Loss', 'MAE Loss', 'SSIM Loss'],
     verbose: bool = False
 ):
-  directory = os.path.join('results', 'generation', 'best')
-  files = os.listdir(directory)
-
-  columns_to_keep = [
-    'Model Name',
-    'Learning Rate',
-    'Down Scale Factor',
-    'Batch Size',
-    'Noise',
-    'Epoch',
-    'BCE Loss',
-    'BCELogits Loss',
-    'MSE Loss',
-    'MAE Loss',
-    'S S I M Loss',
-  ]
-
-  dfs = []
-
-  for f in files:
-    df = pd.read_csv(os.path.join(directory, f))
-    # drop columns that are not in columns_to_keep
-    df = df[df.columns.intersection(columns_to_keep)]
-    # get the record with the lowest MAE Loss
-    df = df[df['MAE Loss'] == df['MAE Loss'].min()]
-
-    dfs.append(df)
-
-  df = pd.concat(dfs)
+  file_path = os.path.join('results', 'generation', 'best', 'Best Variations.csv')
+  df = pd.read_csv(file_path)
 
   for metric in metrics:
     # plot a bar graph of the best generator for each metric
-    plt.figure(figsize=(5, 4))
-    sns.barplot(x='Model Name', y=metric, data=df)
-    plt.title(f'Best Generator for {metric}')
-    plt.xlabel('Model Name')
-    plt.ylabel(metric)
-
-    # use scientific notation for the y-axis
-    plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
-
+    plt.figure(figsize=(21, 18))
+    sns.set(font='Arial')
+    sns.barplot(x='Model Name', y=metric, data=df, palette='magma')
+    plt.title(f'Best Generator for {metric}', fontsize=50)
+    plt.xlabel('Model Name', fontsize=45)
+    plt.ylabel(metric, fontsize=45)
+    
+    # set font size of tick labels
+    plt.xticks(fontsize=35)
+    plt.yticks(fontsize=35)
 
     save_path = os.path.join('images', 'graphs', 'best generator')
     os.makedirs(save_path, exist_ok=True)
